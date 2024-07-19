0ffa9749fc648ee8559ea1126989b5426f03e533
Gustavo Muniz <gustamunizbr@gmail.com>
2024-07-18 23:51:34 -0300

    adds paginated product listing

--- src/main/java/com/munizdev/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/munizdev/dscommerce/dto/ProductDTO.java
@@ -1,10 +1,10 @@
 package com.munizdev.dscommerce.dto;
 
 import com.munizdev.dscommerce.entities.Product;
-import jakarta.persistence.Column;
 
 public class ProductDTO {
 
+    private Long id;
     private String name;
     private String description;
     private Double price;
@@ -14,10 +14,15 @@ public class ProductDTO {
     }
 
     public ProductDTO(Product product) {
+        id = product.getId();
         name = product.getName();
         description = product.getDescription();
         price = product.getPrice();
         imgUrl = product.getImgUrl();
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
--- src/main/java/com/munizdev/dscommerce/services/ProductService.java
+++ src/main/java/com/munizdev/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.munizdev.dscommerce.repositories.ProductRepository;
 import com.munizdev.dscommerce.dto.ProductDTO;
 import com.munizdev.dscommerce.entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,5 +23,12 @@ public class ProductService {
         Optional<Product> result = repository.findById(id);
 
         return new ProductDTO(result.get());
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+
+        return result.map(x -> new ProductDTO(x));
     }
 }

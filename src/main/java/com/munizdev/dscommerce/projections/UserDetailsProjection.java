624539c43ef496dab1b6998e95e1f8038ca4c6a7
Gustavo Muniz <gustamunizbr@gmail.com>
2024-07-29 12:01:53 -0300

    adds UserDetailsService

---
+++ src/main/java/com/munizdev/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package com.munizdev.dscommerce.projections;
+
+public interface UserDetailsProjection {
+
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+}
